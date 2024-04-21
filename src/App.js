4b4516e72ce923e7e50f658f4fb7daad1fe73d70
michaelHoha <michael1hoha@gmail.com>
2024-04-21 21:49:33 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000'); // Connect to the server
 
 function App() {
+  const [usersData, setUsersData] = useState({ users: [] });
+
+  useEffect(() => {
+    // Listen for 'dataUpdate' event from the server
+    socket.on('dataUpdate', (updatedData) => {
+      setUsersData(prevState => ({ ...prevState, users: updatedData.users }));
+    });
+
+    // Clean up event listener on component unmount
+    return () => {
+      socket.off('dataUpdate');
+    };
+  }, []);
+
   return (
     <div className="App">
-      <h1> First commit</h1>
+      {(typeof usersData.users === 'undefined') ?
+        <h1>Loading...</h1>
+        : (usersData.users.map((value, index) => {
+          return (<h1 key={index}>{value}</h1>);
+        }))
+      }
     </div>
   );
 }
