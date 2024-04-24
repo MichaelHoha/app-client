599436e0c2a7932386343368c32a87ef624ce842
michaelHoha <michael1hoha@gmail.com>
2024-04-24 19:41:57 +0300

    add text cruser to the text area

--- src/pages/CodeBlockPage.js
+++ src/pages/CodeBlockPage.js
@@ -4,8 +4,8 @@ import io from "socket.io-client";
 import "./CodeBlockPage.css";
 import debounce from "lodash.debounce";
 import "highlight.js/styles/default.css";
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const socket = io("https://cst-codeblocks-c252dfeccb9b.herokuapp.com");
 
@@ -31,18 +31,16 @@ const CodeBlockPage = () => {
           id: selectedBlockId,
           codeBlockParticipants,
         });
-
-        
       },
       []
     );
 
     // Listen for code changes from the server
     socket.on("codeChange", ({ id, content }) => {
-        setCodeBlock((prevBlock) =>
-          id === selectedBlockId ? { ...prevBlock, content } : prevBlock
-        );
-        });
+      setCodeBlock((prevBlock) =>
+        id === selectedBlockId ? { ...prevBlock, content } : prevBlock
+      );
+    });
 
     // // Listen for code changes from the server
     // socket.on("praticipantesCountDown", ({ id, content }) => {
@@ -71,34 +69,35 @@ const CodeBlockPage = () => {
   }, []);
 
   const handleCodeChange = (content) => {
-// TODO add delay use debounce
+    // TODO add delay use debounce
     socket.emit("codeChange", { id: selectedBlockId, content });
   };
 
   return (
     <div className="code-block-page-style">
-    <h1 className="block-page-title">Code Block</h1>
-    <h2 className="func-title">{codeBlock?.title}</h2>
-    <div style={{ position: 'relative' }}>
-      <SyntaxHighlighter language="javascript" style={docco}>
-        {codeBlock?.content}
-      </SyntaxHighlighter>
-      <textarea
-        className="code-textarea"
-        value={codeBlock?.content}
-        onChange={(e) => handleCodeChange(e.target.value)}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          opacity: 0, // Make the textarea invisible but still clickable/editable
-          zIndex: 1, // Ensure textarea is on top of SyntaxHighlighter
-        }}
-      />
+      <h1 className="block-page-title">Code Block</h1>
+      <h2 className="func-title">{codeBlock?.title}</h2>
+      <div style={{ position: "relative" }}>
+        <SyntaxHighlighter language="javascript" style={docco}>
+          {codeBlock?.content}
+        </SyntaxHighlighter>
+        <textarea
+          className="code-textarea"
+          value={codeBlock?.content}
+          onChange={(e) => handleCodeChange(e.target.value)}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            opacity: 0, // Make the textarea invisible but still clickable/editable
+            zIndex: 1, // Ensure textarea is on top of SyntaxHighlighter
+            cursor: "text", // Set cursor to text
+          }}
+        />
+      </div>
     </div>
-  </div>
   );
 };
 
