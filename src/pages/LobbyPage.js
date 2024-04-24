8f218cba8fc2921147a0b91e7b1af9eec23a3921
michaelHoha <michael1hoha@gmail.com>
2024-04-24 17:03:38 +0300

    test commit

--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -5,21 +5,21 @@ import "./LobbyPage.css";
 const LobbyPage = () => {
   const [codeBlocks, setCodeBlocks] = useState([]);
   const [isLoadibg, setIsLoading] = useState(true);
-  
+
   // here we get the data from "herokuapp/api/codeBlocks" and set it to the codeBlocks state
   useEffect(() => {
     fetch("https://cst-codeblocks-c252dfeccb9b.herokuapp.com/api/codeBlocks")
-    .then((res) => res.json())
-    .then((data) => {
-      setCodeBlocks(data);
-      setIsLoading(false);
-      console.log(data);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        setCodeBlocks(data);
+        setIsLoading(false);
+        console.log(data);
+      });
   }, []);
-  
+
   return (
     <div className="lobby-page">
-      <h1 className="lobby-title">Choose code block</h1>
+      <h1 className="lobby-title">Choose test block</h1>
       {isLoadibg ? (
         <h1>Loading...</h1>
       ) : (
