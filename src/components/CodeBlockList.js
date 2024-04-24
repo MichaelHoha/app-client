680b5367e55a63ad4e040a09569bb291e45107d9
michaelHoha <michael1hoha@gmail.com>
2024-04-24 11:39:09 +0300

    Creating components

--- src/components/CodeBlockList.js
+++ src/components/CodeBlockList.js
@@ -1,2 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
+const CodeBlockList = (props) => {
+  const navigate = useNavigate();
+
+  // open new navigations urls using the useNavigae react-router-dom
+  const handleClick = (id) => navigate("/codeblocks/" + id);
+  return (
+    <div>
+      <ul className="lobby-list">
+        {props.codeBlocks.map((block) => (
+          // TODO export to new component CodeBlockItem
+          <li
+            className="lobby-item"
+            onClick={() => handleClick(block.id)}
+            key={block.id}
+          >
+            <p>{block.title}</p>
+            {/* TODO all styles should be in the css file */}
+            <button style={{ backgroundColor: "transparent", border: 0 }}>
+              <span>&#8594;</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CodeBlockList;
--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -1,50 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-// import io from 'socket.io-client';
+import CodeBlockList from "../components/CodeBlockList";
 import "./LobbyPage.css";
 
 const LobbyPage = () => {
   const [codeBlocks, setCodeBlocks] = useState([]);
   const [isLoadibg, setIsLoading] = useState(true);
-  const navigate = useNavigate();
 
+  
   // here we get the data from "herokuapp/api/codeBlocks" and set it to the codeBlocks state
   useEffect(() => {
     fetch("https://cst-codeblocks-c252dfeccb9b.herokuapp.com/api/codeBlocks")
-      .then((res) => res.json())
-      .then((data) => {
-        setCodeBlocks(data);
-        setIsLoading(false);
-      });
+    .then((res) => res.json())
+    .then((data) => {
+      setCodeBlocks(data);
+      setIsLoading(false);
+    });
   }, []);
-
-  // open new navigations urls using the useNavigae react-router-dom
-  const handleClick = (id) => navigate("/codeblocks/" + id);
-
+  
   return (
     <div className="lobby-page">
       <h1>Choose code block</h1>
       {isLoadibg ? (
         <h1>Loading...</h1>
       ) : (
-        // TODO export to new component CodeBlockList
-        <ul className="lobby-list">
-          {codeBlocks.map((block) => (
-            // TODO export to new component CodeBlockItem
-            <li
-              className="lobby-item"
-              onClick={() => handleClick(block.id)}
-              key={block.id}
-            >
-              <p>{block.title}</p>
-              {/* TODO all styles should be in the css file */}
-              <button style={{ backgroundColor: "transparent", border: 0 }}>
-                <span>&#8594;</span>
-              </button>
-            
-            </li>
-          ))}
-        </ul>
+        <CodeBlockList codeBlocks={codeBlocks} />
       )}
     </div>
   );
